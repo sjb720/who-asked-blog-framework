68fe90fa8e558532c9b526729e59526cab51b356
Stephen Joseph <sjbuonamassa@gmail.com>
2026-02-02 02:02:32 -0500

    fix: entrypoint script for production

--- docker-entrypoint.prod.sh
+++ docker-entrypoint.prod.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running database migrations..."
-npx prisma migrate deploy
+./node_modules/prisma/build/index.js migrate deploy
 
 echo "Starting production server..."
 exec node server.js
